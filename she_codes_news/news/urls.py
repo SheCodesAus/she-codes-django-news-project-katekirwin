f45d646f0ce086ad1444ed6ffb679ec6b19b4c70
katekirwin <kate@spacecubed.com>
2022-12-08 10:27:58 +1100

    Setup Users App

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'news'
@@ -6,4 +6,6 @@ app_name = 'news'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name="story"),
+    path('add-story/', views.AddStoryView.as_view(), name='newStory'),
+    path('users/', include('django.contrib.auth.urls')),
 ]
